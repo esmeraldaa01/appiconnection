ae04cb6a965fb4d9adb213974492b807e892a00b
esmeraldaa01 <lameesmeralda7@gmail.com>
2022-04-01 17:35:26 +0200

    k

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {Input} from "antd";
+import { Input } from "antd";
 
 function App() {
-const [ users , setUsers ] = useState([]);
-const [search ,setSearch] = useState();
+    const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        axios.get('https://api.github.com/users')
-            .then(response => setUsers(response.data));
-    },[])
+        axios.get("https://api.github.com/users").then((response) => {
+            console.log(response.data);
+            setUsers(response.data);
+        });
+    }, []);
 
-
-const handleChange = (e) => {
+    const handleChange = (e) => {
         setSearch(e.target.value);
-        debugger
-}
+        debugger;
+    };
 
-console.log(users, 's')
-
-  return (
-    <div className="App">
-        <Input onChange={ handleChange} placeholder="Basic usage" />
-        <br/>
-        {users.filter((user) => {
-                return (
-                    <div>{Object.values(Object.keys(user.login)).includes(search)}</div>
-                )
-           })}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Input onChange={handleChange} placeholder="Basic usage" />
+            <br />
+            {users
+                .filter((user) => user.login.indexOf(search) !== -1)
+                .map((userIs) => (
+                    <div>{userIs.login}</div>
+                ))}
+        </div>
+    );
 }
 
 export default App;
 
 
+// <div>{Object.values(Object.keys(user.login)).includes(search)}</div>
+// JSON.stringify(Object.values(x)).join('').toLowerCase().includes(inputValue.toLowerCase())
+//user.login.includes(search)
