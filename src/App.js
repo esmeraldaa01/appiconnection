185c57e8423adc70cefc6fc9420dc49cb499fc2f
esmeraldaa01 <lameesmeralda7@gmail.com>
2022-04-01 16:16:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Input} from "antd";
+
+function App() {
+const [ users , setUsers ] = useState([]);
+const [search ,setSearch] = useState();
+
+    useEffect(() => {
+        axios.get('https://api.github.com/users')
+            .then(response => setUsers(response.data));
+    },[])
+
+
+const handleChange = (e) => {
+        setSearch(e.target.value);
+        debugger
+}
+
+console.log(search, 's')
+
+  return (
+    <div className="App">
+        <Input onChange={ handleChange} placeholder="Basic usage" />
+        <br/>
+        {users.filter((user) => {
+                return (
+                    <div>{user.login.includes(search)}</div>
+                )
+           })}
+    </div>
+  );
+}
+
+export default App;
+
